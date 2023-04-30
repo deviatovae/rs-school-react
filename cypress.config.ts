3717da8f3cf51fb9ba2ff2c5e5357def8c18cc40
Alena Deviatova <aedeviatova@yandex.ru>
2023-04-30 20:40:46 +0300

    feat: configure cypress code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'cypress'
+import coverageTask from '@cypress/code-coverage/task'
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      coverageTask(on, config)
+      return config
     },
+    video: false,
+    fixturesFolder: './cypress/fixtures',
+    screenshotOnRunFailure: false,
   },
 })
